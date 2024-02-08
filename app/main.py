b836760b81bba3e18864534f4049b9a7c4c9dbf7
Kjeld <kjeld.oostra@student.uva.nl>
2024-02-08 11:25:45 +0100

    improved stability

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Query
-from typing import Annotated
 from selenium import webdriver
 import html2text
 from pyvirtualdisplay import Display
 from typing import Annotated, Optional
 from openai import OpenAI
+from pydantic import BaseModel
 
 app = FastAPI()
 openai_client = OpenAI()
@@ -12,9 +12,12 @@ openai_client = OpenAI()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1920,1080')
-#chrome_options.add_argument('--headless=new')
+chrome_options.add_argument('--headless=new')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
+
+class Body(BaseModel):
+    body: str
 
 @app.get("/scrape_urls")
 def scrape_urls(urls: Annotated[list[str], Query()], ignore_links = 1, ignore_images = 1):
@@ -38,13 +41,14 @@ def scrape_urls(urls: Annotated[list[str], Query()], ignore_links = 1, ignore_im
         result[url] = h.handle(driver.page_source)
 
     # Kill driver
-    driver.quit()
+    driver.close()
     display.stop()
 
     return result
 
 @app.post("/extract_article")
-def extract_article(body: str):
+def extract_article(body: Body):
+    print(body)
     prompt = f"""I have the contents of a webpage (converted with html2text package to Markdown). This webpage contains a blog post. You need to isolate the blog post and return as a fully marked-up article, with ATX style headings.
 Note that the markup in the original article below may be illogical. Apply ATX style heading markup where you this it makes sense, if the article doesn't provide any.
 
@@ -57,7 +61,7 @@ Web page contents:
 {body}"""
 
     response = openai_client.chat.completions.create(
-        model='gpt-4-1106-preview',
+        model='gpt-3.5-turbo-16k',
         messages=[
             {'role': 'system', 'content': 'You are a helpful assistant.'},
             {'role': 'user', 'content': prompt}
